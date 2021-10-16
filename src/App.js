19cb0b6a697b85f5b2f91bea24b697581d21144a
lonewolf2580 <linusngwee@yahoo.com>
2021-10-16 06:04:38 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar'
-import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
     </BrowserRouter>
   );
